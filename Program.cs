14bfd9a72728af23c49174fdf85ce83bcc853389
varshagandham <1210716153@gitam.in>
2020-07-13 16:31:48 +0530

    initial commit

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace prog2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			int i, m = 0, flag = 0;
+			int n = 3;
+			m = n / 2;
+			if (n == 0 || n == 1)
+			{
+				Console.WriteLine(n + " is not prime number");
+			}
+			else
+			{
+				for (i = 2; i <= m; i++)
+				{
+					if (n % i == 0)
+					{
+						Console.WriteLine(n + " is not prime number");
+						flag = 1;
+						break;
+					}
+				}
+				if (flag == 0)
+				{
+					Console.WriteLine(n + " is prime number");
+				}
+			}
+		}
+	}
+}
